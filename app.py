89e4ea2014379839b12c02f0108e52bf56190df6
Rayjong <rayjong7@gmail.com>
2023-05-16 21:34:40 +1000

    project completed

--- app.py
+++ app.py
@@ -9,7 +9,8 @@ from flask import (
     render_template,
     jsonify,
     request,
-    redirect)
+    redirect,
+    url_for)
 
 # Flask setup
 app = Flask(__name__, static_url_path='/static')
@@ -19,6 +20,7 @@ app = Flask(__name__, static_url_path='/static')
 #################################################
 
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import func
 # 'or' allows us to later switch from 'sqlite' to an external database like 'postgres' easily
 # os.environ is used to access 'environment variables' from the operating system
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', '') or "sqlite:///db.sqlite"
@@ -32,6 +34,17 @@ class client(db.Model):
     __tablename__ = 'clients'
 
     id = db.Column(db.Integer, primary_key=True)
+    insurance_age = db.Column(db.Integer)
+    insurance_bmi = db.Column(db.Integer)
+    insurance_children_no = db.Column(db.Integer)
+    insurance_gender = db.Column(db.String(20))
+    insurance_smoker = db.Column(db.String(20))
+    insurance_region = db.Column(db.String(20))
+    insurance_medical_history = db.Column(db.String(20))
+    insurance_family_medical_history = db.Column(db.String(20))
+    insurance_exercise_frequency = db.Column(db.String(20))
+    insurance_occupation = db.Column(db.String(20))
+    insurance_coverage_level = db.Column(db.String(20))
     insurance_basic = db.Column(db.Float)
     insurance_standard = db.Column(db.Float)
     insurance_premium = db.Column(db.Float)
@@ -39,7 +52,7 @@ class client(db.Model):
 
     def __repr__(self):
         return '<client %r>' % (self.name)
-
+    
 with app.app_context():
     db.create_all()
 
@@ -69,9 +82,21 @@ def about():
 def contact():
     return render_template("contact_us.html")
 
+@app.route("/cover_options")
+def cover_option():
+    return render_template("cover_options.html")
+
 @app.route("/estimator", methods = ['GET','POST'])
 def estimator(): 
     if request.method == 'POST':
+        estimate = []
+
+        #Create a list of categorical form answers that will be entered into estimate/prediction
+        library = {'gender': ['Female', 'Male'], 'smoker': ['No', 'Yes'], 'region': ['Northeast', 'Northwest','Southeast', 'Southwest'], 'medical_history': ['Diabetes', 'Heart disease', 'High blood pressure', 'None'], 'family_medical_history': ['Diabetes', 'Heart disease', 'High blood pressure', 'None'], 'exercise_frequency': ['Frequently', 'Never', 'Occasionally', 'Rarely'], 'occupation': ['Blue collar', 'Student', 'Unemployed', 'White collar'], 'coverage_level': ['Basic', 'Premium', 'Standard']}
+        #identify  the original column headers (also in library - used for lookup
+        column = ['gender','smoker', 'region', 'medical_history', 'family_medical_history', 'exercise_frequency', 'occupation', 'coverage_level']
+    
+        #retrieve data from html form
         client_age = request.form['client_age']
         bmi = request.form['bmi']
         children_no = request.form['children_no']
@@ -83,81 +108,48 @@ def estimator():
         exercise_frequency = request.form['exercise_frequency']
         occupation = request.form['occupation']
         coverage_level = request.form['coverage_level']
-
-        # get dummy data for gender
-        gender_male = 1 if gender == "Male" else 0
-        gender_female = 1 if gender != "Male" else 0
-
-        # get dummy data for smoker
-        smoker_no = 1 if smoker == "No" else 0
-        smoker_yes = 1 if smoker != "No" else 0
-
-        # get dummy data for region
-        region_northeast = 1 if region == "Northeast" else 0
-        region_northwest = 1 if region == "Northwest" else 0
-        region_southeast = 1 if region == "Southeast" else 0
-        region_southwest = 1 if region == "Southwest" else 0
-
-        # get dummy date for client and family medical history
-        medical_history_Diabetes = 1 if medical_history == "Diabetes" else 0
-        medical_history_Heart_disease = 1 if medical_history == "Heart disease" else 0
-        medical_history_High_blood_pressure = 1 if medical_history == "High blood pressure" else 0
-        medical_history_None = 1 if medical_history not in ["Diabetes", "Heart disease", "High blood pressuret"] else 0
-
-        family_medical_history_Diabetes = 1 if family_medical_history == "Diabetes" else 0
-        family_medical_history_Heart_disease = 1 if family_medical_history == "Heart disease" else 0
-        family_medical_history_High_blood_pressure = 1 if family_medical_history == "High blood pressure" else 0
-        family_medical_history_None = 1 if family_medical_history not in ["Diabetes", "Heart disease", "High blood pressuret"] else 0
-
-        # get dummy data for exercise_frequency
-        exercise_frequency_Frequently = 1 if exercise_frequency == "Frequently" else 0
-        exercise_frequency_Occasionally = 1 if exercise_frequency == "Occasionally" else 0
-        exercise_frequency_Rarely = 1 if exercise_frequency == "Rarely" else 0
-        exercise_frequency_Never = 1 if exercise_frequency == "Never" else 0
-
-        # get dummy data for occupation
-        occupation_Blue_collar = 1 if occupation == "Blue collar" else 0
-        occupation_White_collar = 1 if occupation == "White collar" else 0
-        occupation_Student = 1 if occupation == "Student" else 0
-        occupation_Unemployed = 1 if occupation not in ["Blue collar", "White collar", "Student"] else 0
-
-        # get dummy data for coverage_level
-        coverage_level_Premium = 1 if coverage_level == "Premium" else 0
-        coverage_level_Standard = 1 if coverage_level == "Standard" else 0
-        coverage_level_Basic = 1 if coverage_level == "Basic" else 0
-
-        client_data_list_Basic= [client_age, bmi, children_no, gender_female, gender_male, smoker_no, smoker_yes, region_northeast, region_northwest, region_southeast, region_southwest,\
-                                    medical_history_Diabetes, medical_history_Heart_disease, medical_history_High_blood_pressure, medical_history_None, family_medical_history_Diabetes, \
-                                    family_medical_history_Heart_disease, family_medical_history_High_blood_pressure, family_medical_history_None, exercise_frequency_Frequently, \
-                                    exercise_frequency_Never, exercise_frequency_Occasionally, exercise_frequency_Rarely, occupation_Blue_collar, occupation_Student, occupation_Unemployed, \
-                                    occupation_White_collar, 1, 0, 0]
         
-        client_data_list_Standard= [client_age, bmi, children_no, gender_female, gender_male, smoker_no, smoker_yes, region_northeast, region_northwest, region_southeast, region_southwest,\
-                                    medical_history_Diabetes, medical_history_Heart_disease, medical_history_High_blood_pressure, medical_history_None, family_medical_history_Diabetes, \
-                                    family_medical_history_Heart_disease, family_medical_history_High_blood_pressure, family_medical_history_None, exercise_frequency_Frequently, \
-                                    exercise_frequency_Never, exercise_frequency_Occasionally, exercise_frequency_Rarely, occupation_Blue_collar, occupation_Student, occupation_Unemployed, \
-                                    occupation_White_collar, 0, 0, 1]
+        #Create a list of categorical form answers
+        form_input = [gender, smoker, region, medical_history, family_medical_history, exercise_frequency, occupation, coverage_level]
         
-        client_data_list_Premium= [client_age, bmi, children_no, gender_female, gender_male, smoker_no, smoker_yes, region_northeast, region_northwest, region_southeast, region_southwest,\
-                                    medical_history_Diabetes, medical_history_Heart_disease, medical_history_High_blood_pressure, medical_history_None, family_medical_history_Diabetes, \
-                                    family_medical_history_Heart_disease, family_medical_history_High_blood_pressure, family_medical_history_None, exercise_frequency_Frequently, \
-                                    exercise_frequency_Never, exercise_frequency_Occasionally, exercise_frequency_Rarely, occupation_Blue_collar, occupation_Student, occupation_Unemployed, \
-                                    occupation_White_collar, 0, 1, 0]
-
-        client_data_list_Option = [client_age, bmi, children_no, gender_female, gender_male, smoker_no, smoker_yes, region_northeast, region_northwest, region_southeast, region_southwest,\
-                                    medical_history_Diabetes, medical_history_Heart_disease, medical_history_High_blood_pressure, medical_history_None, family_medical_history_Diabetes, \
-                                    family_medical_history_Heart_disease, family_medical_history_High_blood_pressure, family_medical_history_None, exercise_frequency_Frequently, \
-                                    exercise_frequency_Never, exercise_frequency_Occasionally, exercise_frequency_Rarely, occupation_Blue_collar, occupation_Student, occupation_Unemployed, \
-                                    occupation_White_collar, coverage_level_Basic, coverage_level_Standard, coverage_level_Premium]
-   
+        #enter numerical values for estimate/prediction
+        estimate.append(client_age)
+        estimate.append(bmi)
+        estimate.append(children_no)
+        
+        #create numerical values (get_dummies) for categories for estimate/prediction
+        for j in range(len(column)):
+            item = column[j]
+            temp = []
+            for selection in range(len(library[item])):
+                temp.append(0)
+            for i in range(len(library[item])):
+                if library[item][i] == form_input[j]:
+                    temp[i] = 1
+            estimate.extend(temp)
+        
+        # Create corrections for all 3 price options and customer chosen option
+        client_data_list_Basic=estimate.copy()
+        client_data_list_Basic[27:30]=[1, 0, 0]
+            
+        client_data_list_Standard=estimate.copy()
+        client_data_list_Standard[27:30]=[0, 0, 1]
+            
+        client_data_list_Premium=estimate.copy()
+        client_data_list_Premium[27:30]=[0, 1, 0]
+            
+        client_data_list_Option=estimate.copy()
+        
+        # Establish predicted fees using model        
         index1 = model.predict([client_data_list_Basic])
         index2 = model.predict([client_data_list_Standard])
         index3 = model.predict([client_data_list_Premium])
         index4 = model.predict([client_data_list_Option])
         
         # response = jsonify(f"Predicted Insurance Basic: {index1}, Predicted Insurance Standard: {index2}, Predicted Insurance Premium:{index3}")
-
-        client_insurance = client(insurance_basic=index1, insurance_standard=index2, insurance_premium=index3, insurance_option=index4)
+        
+        #Establish data to be uploaded into sqlite DB
+        client_insurance = client(insurance_age = client_age, insurance_bmi = bmi, insurance_children_no = children_no, insurance_gender = gender, insurance_smoker = smoker, insurance_region = region, insurance_medical_history = medical_history, insurance_family_medical_history = family_medical_history, insurance_exercise_frequency = exercise_frequency, insurance_occupation = occupation, insurance_coverage_level = coverage_level, insurance_basic=index1, insurance_standard=index2, insurance_premium=index3, insurance_option=index4)
 
         db.session.add(client_insurance)
         db.session.commit()
@@ -172,7 +164,6 @@ def estimator():
 def analysis():
 
     insurance_results = db.session.query(client.insurance_basic, client.insurance_standard, client.insurance_premium, client.insurance_option).all()
-
      # Create a dictionary from the row data and append to a list
     insurance_basic = [result[0] for result in insurance_results]
     insurance_standard = [result[1] for result in insurance_results]
@@ -180,13 +171,61 @@ def analysis():
     insurance_option = [result[3] for result in insurance_results]
     data_length = len(insurance_results)-1
 
+    #Identify last row of data
     insurance_data = [{'Basic': insurance_basic[data_length], 'Standard': insurance_standard[data_length], 'Premium': insurance_premium[data_length], 'Client_option': insurance_option[data_length]}]
-
     response = jsonify(insurance_data)
+
     # jsonify(f"Predicted Insurance Basic: {response[0]}, Predicted Insurance Standard: {response[1]}, Predicted Insurance Premium:{response[2]}")
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response
 
-if __name__ == '__main__':
-  app.run(debug=True)
+@app.route("/analytics")
+def analytics():
+    
+    # group by and count features and ready for xValue and yValue to do plots
+    regions = db.session.query(client.insurance_region, func.count(client.insurance_region)).group_by(client.insurance_region).all()
+    excercise = db.session.query(client.insurance_exercise_frequency, func.count(client.insurance_exercise_frequency)).group_by(client.insurance_exercise_frequency).all()
+    smoker = db.session.query(client.insurance_smoker, func.count(client.insurance_smoker)).group_by(client.insurance_gender, client.insurance_smoker).all()
+    occupation = db.session.query(client.insurance_occupation, func.count(client.insurance_occupation)).group_by(client.insurance_gender, client.insurance_occupation).all()
+    # medical = db.session.query(client.insurance_medical_history, func.count(client.insurance_medical_history)).groupy_by(client.insurance_medical_history).all()
+    # family_medical = db.session.query(client.insurance_family_medical_history, func.count(client.insurance_family_medical_history)).groupy_by(client.insurance_family_medical_history).all()
 
+    medical = db.session.query(client.insurance_medical_history, func.count(client.insurance_medical_history)).group_by(client.insurance_medical_history).all()
+    family_medical = db.session.query(client.insurance_family_medical_history, func.count(client.insurance_family_medical_history)).group_by(client.insurance_family_medical_history).all()
+
+    reg_ana_x = [result[0] for result in regions]
+    reg_ana_y = [result[1] for result in regions]
+
+    exe_ana_x = [result[0] for result in excercise]
+    exe_count_y = [result[1] for result in excercise]
+
+    smoker_x = [result[0] for result in smoker]
+    smoker_y = [result[1] for result in smoker]
+
+    occupation_x = [result[0] for result in occupation]
+    occupation_y = [result[1] for result in occupation]
+
+    medical_x = [result[0] for result in medical]
+    medical_y = [result[1] for result in medical]
+
+    family_medical_x = [result[0] for result in family_medical]
+    family_medical_y = [result[1] for result in family_medical]
+
+    reg_ana_list = [{'regions': reg_ana_x}, {"count": reg_ana_y}, {'exercise': exe_ana_x}, {'exe_count':exe_count_y}, {'smoker': smoker_x}, {'smoker_count': smoker_y}, {'occupation': occupation_x}, {'occupation_count': occupation_y}, {'medical_history': medical_x}, {'medical_count': medical_y}, {'family_medical_history': family_medical_x}, {'family_medical_count': family_medical_y}]
+    response_reg = jsonify(reg_ana_list)
+
+    response_reg.headers.add('Access-Control-Allow-Origin', '*')
+
+    return response_reg
+
+@app.route("/management")
+def management():
+    return render_template("internal_analysis.html")
+
+@app.route("/password")
+def password():
+    return render_template("password.html")
+
+if __name__ == '__main__':
+  app.run(debug = True)
+  
--- static/js/app_analysis.js
+++ static/js/app_analysis.js
@@ -7,23 +7,32 @@ d3.json(url).then (function(response){
           console.log (response)
 
           // get prediction result based on clients' insurance cover option
-          let ClientValue = [response[0].Client_option]
-
-          document.getElementById("Predict").innerHTML = 'Your estimate insurance price is ' + ClientValue + ' per year.';
+          let ClientValue = Math.ceil(response[0].Client_option/52)
+          console.log(ClientValue)
 
           // chart init
           let xValue = ['Basic', 'Standard', 'Premium'];
         
-          let yValue = [response[0].Basic, response[0].Standard, response[0].Premium]
+          let yValue = [Math.ceil(response[0].Basic/52), Math.ceil(response[0].Standard/52), Math.ceil(response[0].Premium/52)]
+          console.log(yValue)
+
           let color_list = ['rgb(52, 83, 168)', 'rgb(52, 83, 168)', 'rgb(52, 83, 168)']
+          var client_cover
+
           console.log(yValue,color_list)
 
           for (let i = 0; i< yValue.length; i++) {
             if (yValue[i] == ClientValue) {
               color_list[i] = 'rgb(52, 168, 53)';
+              client_cover = xValue[i]
+              console.log(client_cover)
             }
           }
-          
+
+          document.getElementById("Predict").innerHTML = '<p>By selecting the <b>'+ client_cover + '</b> Option,<br>' +
+                                                        'the estimated cost for this choice will be <br>' +
+                                                        '<b>USA$' + ClientValue + '</b> per week.</p>';
+
           // Create a trace for the bar chart
           let trace = {
             x: xValue,
@@ -31,14 +40,24 @@ d3.json(url).then (function(response){
             type: 'bar',
             marker: {
               color: color_list // Set the color of the bars
-            }
+            },
+            text: yValue.map(String), // Convert yValue array to string and assign as text for each bar
+            textposition: 'auto' // Position the text on top of each bar
           };
           
           // Create a layout for the chart
           let layout = {
             title: 'Predicted Insurance Costs',
+            titlefont: {
+              size: 20,
+              color: 'rgb(107, 107, 107)'
+            },
             xaxis: {
               title: 'Insurance Plan',
+              titlefont: {
+                size: 20,
+                color: 'rgb(107, 107, 107)'
+              },
               tickfont: {
                 size: 20,
                 color: 'rgb(107, 107, 107)'
@@ -47,11 +66,11 @@ d3.json(url).then (function(response){
             yaxis: {
               title: 'Insurance Cost',
               titlefont: {
-                size: 16,
+                size: 20,
                 color: 'rgb(107, 107, 107)'
               },
               tickfont: {
-                size: 14,
+                size: 20,
                 color: 'rgb(107, 107, 107)'
               }
             },
